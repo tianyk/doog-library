33d85e367e96f14c55753691a9d0b7178c4e25f0
tyk <yongketian@gmail.com>
2015-05-12 23:38:01 +0800

    添加借阅表表结构

--- sql/schema/init-system-schema.sql
+++ sql/schema/init-system-schema.sql
@@ -7,7 +7,7 @@ use `doog_library`;
 drop table if exists `sys_user`;
 drop table if exists `sys_book`;
 
-##user
+-- 用户表
 create table `sys_user`(
 `user_id` bigint not null auto_increment,
 `username` varchar(100),
@@ -15,7 +15,7 @@ constraint `pk_sys_user` primary key(`user_id`)
 ) charset=utf8 ENGINE=InnoDB;
 alter table `sys_user` auto_increment=100000;
 
-##book
+-- 书籍表
 create table `sys_book`(
 `book_id` bigint not null auto_increment,
 `title` varchar(100) not null,
@@ -25,7 +25,7 @@ constraint `pk_sys_book` primary key(`book_id`)
 ) charset=utf8 ENGINE=InnoDB;
 alter table `sys_book` auto_increment=100000;
 
-##collection
+-- 藏书表
 drop table if exists `sys_collection`;
 create table `sys_collection`(
 `book_id` bigint not null,
@@ -36,3 +36,20 @@ constraint `fk_sys_collection_sys_user_user_id` foreign key (`user_id`) referenc
 constraint `fk_sys_collection_sys_book_book_id` foreign key (`book_id`) references `sys_book` (`book_id`)
 ) charset=utf8 ENGINE=InnoDB;
 alter table `sys_collection` auto_increment=100000;
+
+-- 借阅表
+drop table if exists `sys_brrow_history`;
+create table `sys_brrow_history`(
+`brrow_history_id` bigint not null auto_increment,
+`book_id` bigint not null,
+`owner_user_id` bigint not null,
+`user_id` bigint not null,
+`loan_at` bigint not null,
+`return_at` bigint not null,
+`state` varchar(10) not null default 'reading',
+constraint `pk_sys_brrow_history` primary key(`brrow_history_id`),
+constraint `fk_sys_brrow_history_sys_user_user_id` foreign key (`user_id`) references `sys_user` (`user_id`),
+constraint `fk_sys_brrow_history_sys_user_owner_user_id` foreign key (`owner_user_id`) references `sys_user` (`user_id`),
+constraint `fk_sys_brrow_history_sys_book_book_id` foreign key (`book_id`) references `sys_book` (`book_id`)
+) charset=utf8 ENGINE=InnoDB;
+alter table `sys_brrow_history` auto_increment=100000;
